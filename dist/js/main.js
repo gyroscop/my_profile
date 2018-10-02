642bbc3c5d51a1b28a8232777ad0f3a48929edde
Dibya <dibya@gmail.com>
2018-10-02 12:44:43 +0530

    half day

---
+++ dist/js/main.js
@@ -0,0 +1,7 @@
+//Selection of DOM items
+
+const menuBtn = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-items');
